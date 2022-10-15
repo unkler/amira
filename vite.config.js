b819203459c1761fab9b3eafb94a0091f9c8a5e3
unkler <poru@add-natural.com>
2022-10-15 19:38:51 +0900

    flatpicker追加

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/js/app.js',
+                'resources/js/app.js',
+                'resources/js/flatpickr.js',
             ],
             refresh: [
                 ...refreshPaths,
