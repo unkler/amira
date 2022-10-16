6242d5a49e1842c946e41706cd25fccdb2ba9af3
unkler <poru@add-natural.com>
2022-10-16 20:53:12 +0900

    eventのstore処理追加

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\DB;
 use App\Http\Requests\StoreEventRequest;
 use App\Http\Requests\UpdateEventRequest;
 use App\Models\Event;
+use App\Services\EventService;
 
 class EventController extends Controller
 {
@@ -39,7 +40,30 @@ class EventController extends Controller
      */
     public function store(StoreEventRequest $request)
     {
-        //
+
+        $check = EventService::checkEventDuplication($request['event_date'], $request['start_time'], $request['end_time']);
+
+        if($check) {
+            session()->flash('status', 'この時間帯は既に他の予約が存在します');
+            return view('manager.events.create'); 
+        }
+
+        $startDate = EventService::joinDateAndTime($request['event_date'], $request['start_time']);
+        $endDate = EventService::joinDateAndTime($request['event_date'], $request['end_time']);
+        
+        Event::create([
+            'name' => $request['event_name'],
+            'information' => $request['information'],
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'max_people' => $request['max_people'],
+            'is_visible' => $request['is_visible'],
+        ]);
+
+        session()->flash('status', '登録しました');
+
+        return to_route('events.index');
+        
     }
 
     /**
