30bfaf86b0e9dae77b92740a4c85b7f03a80835e
unkler <poru@add-natural.com>
2022-10-16 23:54:01 +0900

    eventのshow処理追加

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -74,7 +74,13 @@ class EventController extends Controller
      */
     public function show(Event $event)
     {
-        //
+        $event = Event::findOrFail($event->id);
+        $eventDate = $event->eventDate;
+        $startTime = $event->startTime;
+        $endTime = $event->endTime;
+
+        return view('manager.events.show',
+            compact('event', 'eventDate', 'startTime', 'endTime'));
     }
 
     /**
--- resources/views/manager/events/show.blade.php
+++ resources/views/manager/events/show.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            イベント新規登録
+            イベント詳細
         </h2>
     </x-slot>
 
@@ -17,47 +17,47 @@
                         </div>
                     @endif
 
-                    <form method="POST" action="{{ route('events.store') }}">
-                        @csrf
+                    <form method="GET" action="{{ route('events.edit', ['event' => $event->id]) }}">
 
                         <div>
                             <x-jet-label for="event_name" value="イベント名" />
-                            <x-jet-input id="event_name" class="block mt-1 w-full" type="text" name="event_name" :value="old('event_name')" required autofocus />
+                            {{ $event->name }}
                         </div>
 
                         <div class="mt-4">
                             <x-jet-label for="information" value="イベント詳細" />
-                            <x-textarea row="3" id="information" name="information" class="block mt-1 w-full">{{ old('information') }}</x-textarea>
+                            {!! nl2br(e($event->information)) !!}
                         </div>
 
                         <div class="md:flex justify-between">
                             <div class="mt-4">
                                 <x-jet-label for="event_date" value="イベント日付" />
-                                <x-jet-input id="event_date" class="block mt-1 w-full" type="text" name="event_date" :value="old('event_date')" required />
+                                {{ $event->eventDate }}
                             </div>
 
                             <div class="mt-4">
                                 <x-jet-label for="start_time" value="開始時間" />
-                                <x-jet-input id="start_time" class="block mt-1 w-full" type="text" name="start_time" :value="old('start_time')" required />
+                                {{ $event->startTime }}
                             </div>
 
                             <div class="mt-4">
                                 <x-jet-label for="end_time" value="終了時間" />
-                                <x-jet-input id="end_time" class="block mt-1 w-full" type="text" name="end_time" :value="old('end_time')" required />
+                                {{ $event->endTime }}
                             </div>
                         </div>
 
                         <div class="md:flex justify-between items-end">
                             <div class="mt-4">
                                 <x-jet-label for="max_people" value="定員数" />
-                                <x-jet-input id="max_people" class="block mt-1 w-full" type="number" name="max_people" :value="old('max_people')" required />
+                                {{ $event->max_people }}
                             </div>
-                            <div class="flex space-x-4 justify-around">
-                                <input type="radio" name="is_visible" value="1" checked />表示
-                                <input type="radio" name="is_visible" value="0" />非表示
-                            </div>
+                            @if($event->is_visible)
+                                表示
+                            @else
+                                非表示
+                            @endif
                             <x-jet-button class="ml-4">
-                                新規登録
+                                編集
                             </x-jet-button>
                         </div>
                     </form>
