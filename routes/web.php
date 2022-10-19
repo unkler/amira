4f8943cf832cd627f9a8799827650ccef7450e8e
unkler <poru@add-natural.com>
2022-10-20 00:45:04 +0900

    イベントの予約処理を追加

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LivewireTestController;
 use App\Http\Controllers\AlpineTestController;
 use App\Http\Controllers\EventController;
+use App\Http\Controllers\ReservationController;
 use Barryvdh\Debugbar\DataCollector\EventCollector;
 
 /*
@@ -21,15 +22,15 @@ Route::get('/', function () {
     return view('calendar');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
 
 Route::prefix('manager')
     ->middleware('can:manager-higher')
@@ -37,11 +38,12 @@ Route::prefix('manager')
         Route::get('events/past', [EventController::class, 'past'])->name('events.past');
         Route::resource('events', EventController::class);
     });
+
 Route::middleware('can:user-higher')
     ->group(function() {
-        Route::get('index', function() {
-            dd('user');
-        });
+        Route::get('/dashboard', [ReservationController::class, 'dashboard'])->name('dashboard');
+        Route::get('/{id}', [ReservationController::class, 'detail'])->name('events.detail');
+        Route::post('/{id}', [ReservationController::class, 'reserve'])->name('events.reserve');
     });
 
 Route::controller(LivewireTestController::class)
